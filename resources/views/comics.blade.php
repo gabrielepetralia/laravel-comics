a2cf16ef08248adde069e0f853bda041fbfaf6cc
gabrielepetralia <gabri.petralia03@gmail.com>
2023-06-06 01:57:15 +0200

    [add] comics page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -5,5 +5,25 @@
 @endsection
 
 @section("content")
-    <h1>Comics</h1>
+    <div class="comics">
+        <div class="gp-container">
+            <h1 class="fs-xxl">Current Series</h1>
+
+            <div class="cards-container">
+                @foreach ($comics as $comic)
+                <div class="gp-card">
+                    <div class="thumb">
+                    <img src="{{ $comic["thumb"] }}" alt="{{ $comic["series"] }}">
+                    </div>
+                    <h5 class="fs-md fw-light">{{ $comic["series"] }}</h5>
+                </div>
+                @endforeach
+            </div>
+
+            <div class="load">
+                <button class="fs-xs fw-semibold">Load More</button>
+            </div>
+
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('comics');
+    $comics=config("comics");
+    return view('comics',compact("comics"));
 })->name("comics");
 
 Route::get('/characters', function () {
